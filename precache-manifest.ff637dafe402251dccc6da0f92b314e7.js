fe007b454ea51e0973267a57b833f9336404d958
millerpractice <sarahla@abv.bg>
2020-01-04 11:55:50 +0100

    Updates

---
+++ precache-manifest.ff637dafe402251dccc6da0f92b314e7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ec56b25f218e117f27d45a199f5bdf3",
+    "url": "/practicemill/index.html"
+  },
+  {
+    "revision": "32600ff8bf02a2e29495",
+    "url": "/practicemill/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c4361072378da8f9e4b0",
+    "url": "/practicemill/static/js/2.ce535ae6.chunk.js"
+  },
+  {
+    "revision": "a0560e08c2bea09cecbe463a56dcf52b",
+    "url": "/practicemill/static/js/2.ce535ae6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "32600ff8bf02a2e29495",
+    "url": "/practicemill/static/js/main.3f0dd560.chunk.js"
+  },
+  {
+    "revision": "cc97867ca90824473e18",
+    "url": "/practicemill/static/js/runtime-main.12e3730b.js"
+  },
+  {
+    "revision": "de578f1b6cbbb6ff9ae136a6cf0000b3",
+    "url": "/practicemill/static/media/arzt.de578f1b.png"
+  },
+  {
+    "revision": "3f35d1394483ca050ff7cbf34b177359",
+    "url": "/practicemill/static/media/healthcare.3f35d139.png"
+  },
+  {
+    "revision": "0f807958568a1a23afd33663356e317d",
+    "url": "/practicemill/static/media/hearth.0f807958.png"
+  },
+  {
+    "revision": "692727752824ed1f880389f1bbc5fd44",
+    "url": "/practicemill/static/media/logo.69272775.png"
+  },
+  {
+    "revision": "b34c85bdeb564a07942c3bc651886f2c",
+    "url": "/practicemill/static/media/map.b34c85bd.PNG"
+  },
+  {
+    "revision": "9c84c66ca647ca56c7a35f09efb55aca",
+    "url": "/practicemill/static/media/office.9c84c66c.jpg"
+  },
+  {
+    "revision": "fb4b3d6736b3134d595629430fc2b809",
+    "url": "/practicemill/static/media/welcome.fb4b3d67.jpg"
+  }
+]);
